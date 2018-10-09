0ff1c2511f75bfd8cba3c7417eb6eaff297b8262
jy <johniao@163.com>
2018-10-09 15:15:11 +0800

    w1

--- train_eval.py
+++ train_eval.py
@@ -22,10 +22,10 @@ if __name__ == '__main__':
 
     for i in range(30):
        # train 1 epoch
-        print('################    train    ################')
-        p = os.popen('python ./train.py' + cmd)
-        for l in p:
-            print(l.strip())
+        # print('################    train    ################')
+        # p = os.popen('python ./train.py' + cmd)
+        # for l in p:
+        #     print(l.strip())
 
         # eval
         print('################    eval    ################')
